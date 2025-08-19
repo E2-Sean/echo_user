86064dcf6cedc9ddb85492aa0c7dda51ed60ea8d
Sean Welsh <sean@engineno2.com>
2025-08-19 15:58:44 +1000

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def whoami(request):
+    # Header arrives as HTTP_X_REMOTE_USER in WSGI environ
+    user = request.META.get('HTTP_X_REMOTE_USER', '<unknown>')
+    return HttpResponse(f"Logged in as: {user}")
