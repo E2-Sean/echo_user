813ffee3bfef770f535c7a2ac92b04b5957520a3
Sean Welsh <sean@engineno2.com>
2025-08-19 16:02:00 +1000

    test comment

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#test comment
+
 def whoami(request):
     # Header arrives as HTTP_X_REMOTE_USER in WSGI environ
     user = request.META.get('HTTP_X_REMOTE_USER', '<unknown>')
